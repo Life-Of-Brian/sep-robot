b5793ae3fead27f01d0e0b3a7e7954f3d82fe636
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2021-11-29 13:18:44 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    cuteBot.moveTime(cuteBot.Direction.forward, 75, 5)
+    basic.pause(1000)
+    cuteBot.moveTime(cuteBot.Direction.forward, 100, 5)
+    basic.pause(1000)
+})
 basic.forever(function () {
 	
 })
